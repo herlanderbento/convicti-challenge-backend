142780b571547f4320b8b09cce8a713b9919e60f
Herlander Bento <herlanderbento19@gmail.com>
2022-12-26 07:29:28 +0100

    removendo autenticacao  general-director

--- src/shared/infra/http/routes/authenticate.routes.ts
+++ src/shared/infra/http/routes/authenticate.routes.ts
@@ -1,17 +1,10 @@
 import { AuthenticateUserController } from '@app/modules/accounts/use-cases/authenticate-users/authenticate-user-controller';
-import { AuthenticateGeneralDirectorController } from '@app/modules/general-director/use-cases/authenticate-general-director/authenticate-general-director-controller';
+
 import { Router } from 'express';
 
 const authenticateRoutes = Router();
-const authenticateGeneralDirectorController =
-  new AuthenticateGeneralDirectorController();
 
 const authenticateUserController = new AuthenticateUserController();
-
-authenticateRoutes.post(
-  '/general-director',
-  authenticateGeneralDirectorController.handle
-);
 
 authenticateRoutes.post('/', authenticateUserController.handle);
 
